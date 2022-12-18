c345849ffd4d6d794da1ad3465d5d74d0eef4581
Yujin <jini1514@soongsil.ac.kr>
2022-12-18 21:27:55 +0900

    fix: app-> firstPage로 기능 전환

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navigate,Link } from "react-router-dom";
+import { Navigate,Link,Routes,Router,Route,Switch } from "react-router-dom";
+import login from './login';
+import FirstPage from './firstPage';
 
 //Class 형태로 만들어진 컴포넌트는 꼭 render 함수 있어야하며 render 함수 내부에서 JSX 코드를 return 해주어야함.
 class App extends Component {
   //render 함수
   render() {
-
     return (
-    
       //JSX
       <div>
-        <div>융학사</div>
-        <img className="firstImg" src="img/first.png" alt="first pic."></img>
-        <button size="large">로그인</button>
-        </div>
+        <FirstPage/>
+      </div>
     );
   }
 }
--- my-app/src/firstPage.js
+++ my-app/src/firstPage.js
@@ -4,19 +4,11 @@ import { Navigate,Link } from "react-router-dom";
  import login from './login';
 
 const firstPage = () => {
-  const navigate = Navigate();
-  const navigateToLogin= () => {
-    navigate("/login");
-  };
-
- 
   return (
     <div>
       <br />
       <img className="firstImg" src="img/first.png" alt="first pic."></img>
-      <Link to="/login">
-          <button size="large">
-            로그인</button></Link>
+      <button>로그인</button>
     </div>
   );
 };

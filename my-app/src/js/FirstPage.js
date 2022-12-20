3aaf39753ad981a0d71c8f3d1b9af72458380b0a
Yujin <jini1514@soongsil.ac.kr>
2022-12-20 13:30:51 +0900

    feat: 첫번째 화면디자인!

--- my-app/src/js/FirstPage.js
+++ my-app/src/js/FirstPage.js
@@ -15,22 +15,24 @@ const FirstPage = () => {
   // 버튼 클릭시 호출
 
   return (
-    <div className="main">
+    <div id="main">
       <br />
-      <div className="login_left">
-        <img src="img/first.png" alt="first pic."></img>
+      <div>
+        <img className="login_left" src="img/first.png" alt="first pic."></img>
       </div>
       <div className="login_right">
         <img className="logo" src="img/ssu-logo.png" alt="logo pic."></img>
+        <button className="goLogin">로그인</button>
         <div className="firstComment">
-          숭실대학교 유세인트의 아이디, 비빌번호를 입력해주시면 됩니다.
+          숭실대학교 유세인트의 <br/>
+          아이디, 비빌번호를 입력해주시면 됩니다.
         </div>
         <div className="secondComment">
           <span>Open Grade / 융학사</span>
           <br />
           융합특성화 자유전공학부 학생들의 성공적인 전과를 위해 만든
-          시스템입니다. 수집한 개인정보는 절대로 개인의 이익을 위해 착복하지
-          않을 것을 약속드립니다. 성공적인 전과를 바라겠습니다.
+          시스템입니다. <br/>수집한 개인정보는 절대로 개인의 이익을 위해 착복하지
+          않을 것을 약속드립니다. <br/>성공적인 전과를 바라겠습니다.
         </div>
       </div>
     </div>

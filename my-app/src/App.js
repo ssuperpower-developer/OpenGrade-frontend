2280dd78599082f4da495c479bc5dfcdee4ddd28
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-12-22 16:22:45 +0900

    x

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import FirstPage from "./js/FirstPage.js";
 // import LoginPage from "./js/LoginPage.js";
 import LoginForm from "./js/LoginPage.js";
 import MainPage from "./js/MainPage";
+import CheckDepartment from "./js/CheckDepartment";
 
 //Class 형태로 만들어진 컴포넌트는 꼭 render 함수 있어야하며 render 함수 내부에서 JSX 코드를 return 해주어야함.
 class App extends Component {
@@ -15,7 +16,8 @@ class App extends Component {
         <Routes>
           <Route path="/" element={<FirstPage />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route path="/login/mainPage" element={<MainPage />} />
+          <Route path="/mainPage" element={<MainPage />} />
+          <Route path="/checkDepartment" element={<CheckDepartment />} />
         </Routes>
       </BrowserRouter>
     );

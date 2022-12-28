c4dcd31c53a830e72d09f79c52462f2ab310a30e
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-12-29 06:08:44 +0900

    feat : api get ranking

--- my-app/src/js/MainPage.js
+++ my-app/src/js/MainPage.js
@@ -1,51 +1,98 @@
 import "../css/MainPage.css";
 import MainBtn from "./MainBtn";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ResponsiveScatterPlot } from "@nivo/scatterplot";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 export default function MainPage() {
+  const scoreName = ["list.softScore", "list.comScore"];
+  const getData = async (department, resultControl) => {
+    try {
+      const api = axios.create({
+        baseURL: "http://34.64.123.6:8080",
+        headers: {
+          jwtToken: localStorage.getItem("wtw-token"),
+        },
+      });
+      let result;
+      console.log("hi");
+      if (resultControl < 2) {
+        result = await api.get(`/result/allranking/${department}`);
+        console.log("hihi");
+      } else {
+        result = await api.get(`/result/applyranking/${department}`);
+        console.log("hihi");
+      }
+      setGetList(result.data.allUsers);
+      // console.log(result.data.allUsers);
+      // result.data.allUsers.map((score, idx) => {
+      //   // data[0].data.push({
+      //   //   x: idx + 1,
+      //   //   y: scoreName[resultControl % 2],
+      //   // });
+
+      // });
+      console.log(result.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const { state } = useLocation();
   const [isPressBtn, setIsPressBtn] = useState([true, false, false, false]);
+  const departmentEng = ["S", "C"];
+  const [btnIdx, setBtnIdx] = useState();
   const handlePressBtn = (index) => {
+    setBtnIdx(index);
     const newBtnList = Array(4).fill(false);
     newBtnList[index] = true;
+    getData(departmentEng[index % 2], index);
     setIsPressBtn(newBtnList);
   };
 
-  // const [nickName, setNickName] = useState();
-  // const handleNickName = () => {
-  //   setNickName(localStorage.getItem("nickName"));
-  // };
-
-  // useEffect(() => {
-  //   handleNickName();
-  // });
+  const [getList, setGetList] = useState([]);
+  const [userNumber, setUserNumber] = useState();
+  useEffect(() => {
+    getList?.map((score, idx) => {
+      if (score.nickname === nickName) {
+        setUserNumber(idx + 1);
+      }
+    });
+    // console.log(getList);
+    // getList?.map((list, idx) => {
+    //   data[0].data.push({
+    //     x: idx + 1,
+    //     // y: ,
+    //     y: list?.softScore,
+    //   });
+    // });
+    // console.log(data[0].data);
+  });
 
   const nickName = localStorage.getItem("nickName");
 
   // 차트 데이터
-  const data = [
-    {
-      id: "전체성적",
-      data: [
-        {
-          x: 68,
-          y: 112,
-        },
-        {
-          x: 37,
-          y: 120,
-        },
-      ],
-    },
-    {
-      id: "내 성적",
-      data: [
-        {
-          x: 95,
-          y: 39,
-        },
-      ],
-    },
+  // let data = [
+  //   {
+  //     id: "전체성적",
+  //     data: [],
+  //   },
+  //   {
+  //     id: "내 성적",
+  //     data: [
+  //       {
+  //         x: userNumber,
+  //         y: getList[userNumber - 1]?.soft_score,
+  //       },
+  //     ],
+  //   },
+  // ];
+
+  const departmentList = [
+    "융합특성화자유전공학부",
+    "소프트웨어학부 지원자",
+    "컴퓨터학부 지원자",
   ];
 
   return (
@@ -55,11 +102,12 @@ export default function MainPage() {
         <span id="titleText">님의 순위결과 조회입니다.</span>
       </div>
       <div id="main">
-        <div id="right">
+        <div id="right">차트 넣을 예정</div>
+        {/* <div id="right">
           <ResponsiveScatterPlot
             data={data}
             margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
-            xScale={{ type: "linear", min: 0, max: "auto" }}
+            xScale={{ type: "point", min: 0, max: "auto" }}
             xFormat=">-.2f"
             yScale={{ type: "linear", min: 0, max: "auto" }}
             yFormat=">-.2f"
@@ -72,18 +120,18 @@ export default function MainPage() {
               tickSize: 5,
               tickPadding: 5,
               tickRotation: 0,
-              legend: "weight",
+              legend: "등수",
               legendPosition: "middle",
-              legendOffset: 46,
+              legendOffset: 50,
             }}
             axisLeft={{
               orient: "left",
               tickSize: 5,
               tickPadding: 5,
               tickRotation: 0,
-              legend: "size",
+              legend: "성적",
               legendPosition: "middle",
-              legendOffset: -60,
+              legendOffset: -50,
             }}
             legends={[
               {
@@ -109,7 +157,7 @@ export default function MainPage() {
               },
             ]}
           />
-        </div>
+        </div> */}
         <div id="left">
           <div>
             <div className="mainTitle">성적 산출 등수</div>
@@ -128,27 +176,37 @@ export default function MainPage() {
           </div>
           <div>
             <div className="mainTitle">실지원 등수</div>
-            <MainBtn
-              btnText={"소프트웨어학부"}
-              index={2}
-              handlePress={handlePressBtn}
-              isSelected={isPressBtn}
-            />
-            <MainBtn
-              btnText={"컴퓨터학부"}
-              index={3}
-              handlePress={handlePressBtn}
-              isSelected={isPressBtn}
-            />
+            {state === "S" ? (
+              <MainBtn
+                btnText={"소프트웨어학부"}
+                index={2}
+                handlePress={handlePressBtn}
+                isSelected={isPressBtn}
+              />
+            ) : (
+              <MainBtn
+                btnText={"컴퓨터학부"}
+                index={3}
+                handlePress={handlePressBtn}
+                isSelected={isPressBtn}
+              />
+            )}
           </div>
           <div id="ranking">
             <span>전체 등수</span>
-            <span>17등 / 150명</span>
+            <span>
+              {userNumber}등 / {getList.length}명
+            </span>
           </div>
           <div id="rankingTextBox">
             <span>
-              <span id="nicknameInBox">"{nickName}"</span>님의 순위는 전체
-              융합특성화자유전공학부 150명 중 17등으로 예상됩니다.
+              <span id="nicknameInBox">"{nickName}"</span>님의 순위는 전체{" "}
+              {btnIdx === 0 || btnIdx === 1
+                ? departmentList[0]
+                : btnIdx === 2
+                ? departmentList[1]
+                : departmentList[2]}{" "}
+              {getList.length}명 중 {userNumber}등으로 예상됩니다.
             </span>
           </div>
         </div>
